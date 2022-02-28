19534edc64711b06b5e281140e7310fe1aa77557
The Rubyist <49131888+ruby-rust-perl@users.noreply.github.com>
2022-02-28 21:30:30 +0530

    Instructions to install `oh-my-posh` via `brew`

--- Linux/ohmyposh.sh
+++ Linux/ohmyposh.sh
@@ -1,10 +1,13 @@
-sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
-sudo chmod +x /usr/local/bin/oh-my-posh
+# Installing Homebrew
+command -p brew 2>/dev/null || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Installing oh-my-posh via Homebrew
+brew tap jandedobbeleer/oh-my-posh
+brew install oh-my-posh
 mkdir ~/.poshthemes
 wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
 unzip ~/.poshthemes/themes.zip -d ~/.poshthemes
 chmod u+rw ~/.poshthemes/*.json
-rm ~/.poshthemes/themes.zip
+rm -f ~/.poshthemes/themes.zip
 
 # View all the themes
 for file in ~/.poshthemes/*.omp.json; do echo "$file\n"; oh-my-posh --config $file --shell universal; echo "\n"; done;
@@ -14,4 +17,4 @@ for file in ~/.poshthemes/*.omp.json; do echo "$file\n"; oh-my-posh --config $fi
 ls ~/.poshthemes > themes.txt
 theme=$(cat themes.txt | shuf -n 1)
 eval "$(oh-my-posh --init --shell bash --config ~/.poshthemes/$theme)"
-rm themes.txt
+rm themes.txt -f
