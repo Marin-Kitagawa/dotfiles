b4f842a5080557ae36c1ceb41342d4fee9bed558
Shenhe <49131888+SuzunaMinami@users.noreply.github.com>
2022-09-22 03:26:39 +0530

    Updated Linux Scripts

---
+++ Linux/rust_coreutils.sh
@@ -0,0 +1,30 @@
+cargo install sd hyperfine du-dust dutree ytop bottom procs skim bat exa fd-find starship ripgrep tokei tealdeer bandwhich grex rmesg zoxide git-delta tp-note nu sd
+########################################
+#                                      #
+#                                      #
+#    ripgrep on Windows choco/scoop    #
+#                                      #
+#                                      #
+########################################
+# sd  : `sed` command alternative and it uses normal regex just like any other programming language
+# hyperfine  : Alternative to `time` command. This is used for benchmarking the time used by each command
+# du-dust  : Alternative to disk utility `du` command
+# dutree  : Tree with size info
+# ytop  : `htop` alternative
+# bottom  : `htop` alternative
+# procs  : Alternative to `ps`. This enables a user to see the processes in more detail
+# skim  : Fuzzy finder and an interactive alternative to `grep`
+# bat  : clone of `cat` command but with syntax highlighting
+# exa  : Modern alternative to `ls` command
+# fd  : Alternative to find (fuzzy)
+# starship  : A very fast and highly customizable prompt for any shell
+# ripgrep  : Alternative to grep
+# tokei  : Lists the languages, sloc in each directory
+# tealdeer  : Modern manpages with more info
+# bandwhich  : Bandwidth display
+# grex  : Test cases to regex
+# rmesg  : Kernel message logger
+# zoxide  : Jump from any directory to any other
+# delta  : Git-difference
+# tp-note  : Clipboard to save-as-note
+# nu  : New shell
