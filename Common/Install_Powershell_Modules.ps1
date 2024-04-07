c7dcf33ef5701538fe2a6c087310cab65ff2cf8b
Marin Kitagawa <49131888+Marin-Kitagawa@users.noreply.github.com>
2024-04-07 13:44:06 +0530

    Update Install_Powershell_Modules.ps1

--- Common/Install_Powershell_Modules.ps1
+++ Common/Install_Powershell_Modules.ps1
@@ -2,3 +2,4 @@ Install-Module posh-git
 winget install JanDeDobbeleer.OhMyPosh
 Install-Module -Name PSReadLine -Force -AllowPrerelease -SkipPublisherCheck
 Install-Module Terminal-Icons -Repository PSGallery
+winget install junegunn.fzf sharkdp.bat BurntSushi.ripgrep.MSVC
